69d48a1fc593fd44d5bfdbfd4d019c0470027b0a
mstf7249 <mbear55771@gmail.com>
2020-12-13 18:37:50 +0800

    bug fixed

--- xgboost/main.py
+++ xgboost/main.py
@@ -8,12 +8,12 @@ import csv
 # preprocessing
 data_train = pd.read_csv('../data/train.csv')
 
-pp.feature_delete(df, pp.del_features)
+pp.feature_delete(data_train, pp.del_features)
 pp.feature_standardize(data_train, pp.stand_features)
 pp.feature_makeNaN(data_train, pp.makeNaN_features)
 pp.feature_makeOthers(data_train, pp.makeOthers_list)
 pp.feature_one_hot(data_train, pp.one_hot_list)
-pp.feature_merge_two_multi(df, 'is_canceled', 'adr', 'adr_real')
+pp.feature_merge_two_multi(data_train, 'is_canceled', 'adr', 'adr_real')
 
 data_train = data_train.dropna()
 data_train = data_train.sample(frac=1) # shuffle
