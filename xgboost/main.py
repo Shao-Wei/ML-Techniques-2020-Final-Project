d13ed469ff7b8c76218f007c4661f01d8f40aebc
mstf7249 <mbear55771@gmail.com>
2020-12-15 00:18:12 +0800

    xgboost initial version

--- xgboost/main.py
+++ xgboost/main.py
@@ -2,19 +2,106 @@ import numpy as np
 import pandas as pd
 import math
 import preprocessing as pp
+import util
 import xgboost as xgb
 import csv
 
 # preprocessing
+print("start preprocessing ...")
+#pp.preprocessing_adr()
+#pp.preprocessing_revenue()
+print("preprocessing done ...")
+
+# train adr
+print("start training adr ...")
+data_adr = xgb.DMatrix('../data/train_pre.csv?format=csv&label_column=0')
+data_adr_noshuffle = xgb.DMatrix('../data/train_pre_noshuffle.csv?format=csv&label_column=0')
+k = 5 # k-fold
+nX = data_adr.num_row()
+nValid = int(data_adr.num_row()*(1/k))
+fold = [[] for _ in range(k)]
+for i in range(k):
+    if (i == k - 1):
+        fold[i] = fold[i] + list(range(nValid*i, nX))
+    else:
+        fold[i] = fold[i] + list(range(nValid*i, nValid*(i+1)))
+
+algs = [[3, 1], [4, 1], [5, 1], [6, 1], [7, 1]] # [max_depth, eta]
+nAlg = len(algs)
+E_algs = [0] * nAlg
+num_round = 10
+for i in range(k):
+    validInd = fold[i]
+    trainInd = []
+    for j in range(k):
+        if (j != i):
+            trainInd = trainInd + fold[j]
+    dtrain = data_adr.slice(trainInd)
+    dvalid = data_adr.slice(validInd)
+    
+    for j in range(nAlg):
+        param = {'max_depth': algs[j][0], 'eta': algs[j][1], 'objective':'reg:squarederror'}
+        bst = xgb.train(param, dtrain, num_round)
+        preds = bst.predict(dvalid)
+        E_algs[j] = E_algs[j] + util.squaredError(dvalid.get_label(), preds)
+
+E_algs[:] = [x/k for x in E_algs]
+bestAlg = util.get_bestAlg(algs, E_algs)
+param = {'max_depth': bestAlg[0], 'eta': bestAlg[1], 'objective':'reg:squarederror'}
+bst = xgb.train(param, data_adr, num_round)
+adr_preds = bst.predict(data_adr_noshuffle)
+
+# calculate revenue from adr_preds
 data_train = pd.read_csv('../data/train.csv')
+revenue_predict = util.get_revenue_predict(data_train, adr_preds)
+print("training adr done ...")
 
-pp.feature_delete(data_train, pp.del_features)
-pp.feature_standardize(data_train, pp.stand_features)
-pp.feature_makeNaN(data_train, pp.makeNaN_features)
-pp.feature_makeOthers(data_train, pp.makeOthers_list)
-pp.feature_one_hot(data_train, pp.one_hot_list)
-pp.feature_merge_two_multi(data_train, 'is_canceled', 'adr', 'adr_real')
+# train quantization
+print("start training quantization ...")
+data_quan = xgb.DMatrix('../data/train_label_label_no_quan.csv?format=csv&label_column=0')
+k = 1 # k-fold
+nX = data_quan.num_row()
+nValid = int(data_quan.num_row()*(1/k))
+fold = [[] for _ in range(k)]
+for i in range(k):
+    if (i == k - 1):
+        fold[i] = fold[i] + list(range(nValid*i, nX))
+    else:
+        fold[i] = fold[i] + list(range(nValid*i, nValid*(i+1)))
 
-data_train = data_train.dropna()
-data_train = data_train.sample(frac=1) # shuffle
-data_train.to_csv('../data/train_pre.csv', index=False)
+algs = [[3, 1], [4, 1], [5, 1], [6, 1], [7, 1]] # [max_depth, eta]
+nAlg = len(algs)
+E_algs = [0] * nAlg
+num_round = 10
+for i in range(k):
+    validInd = fold[i]
+    trainInd = []
+    for j in range(k):
+        if (j != i):
+            trainInd = trainInd + fold[j]
+    #dtrain = data_quan.slice(trainInd) # k > 1 
+    dtrain = data_quan.slice(validInd) # k = 1: use all training data
+    dvalid = data_quan.slice(validInd)
+    #num_class = np.amax(dtrain.get_label()) - np.amin(dtrain.get_label()) + 1
+    
+    for j in range(nAlg):
+        param = {'max_depth': algs[j][0], 'eta': algs[j][1], 'objective':'multi:softmax',  'num_class': 10, 'eval_metric':'mae'}
+        bst = xgb.train(param, dtrain, num_round)
+        preds = bst.predict(dvalid)
+        E_algs[j] = E_algs[j] + util.L1_Error(dvalid.get_label(), preds)
+
+E_algs[:] = [x/k for x in E_algs]
+bestAlg = util.get_bestAlg(algs, E_algs)
+param = {'max_depth': bestAlg[0], 'eta': bestAlg[1], 'objective':'multi:softmax',  'num_class': 10, 'eval_metric':'mae'}
+bst = xgb.train(param, data_quan, num_round)
+print("training quantization done ...")
+
+# calculate final error
+data_error = pd.read_csv('../data/train_label.csv')
+pp.feature_delete_one(data_error, 'arrival_date')
+data_error.insert(len(data_error.columns), 'label_predict', revenue_predict, True) 
+data_error.to_csv('../data/train_label_predict.csv', index=False, header=False)
+data_error = xgb.DMatrix('../data/train_label_predict.csv?format=csv&label_column=0')
+preds = bst.predict(data_error)
+E = util.L1_Error(data_error.get_label(), preds)
+print("final E_train = ", E, sep='')
--- xgboost/preprocessing.py
+++ xgboost/preprocessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import util
 
 del_features = ['ID', 
                 #'is_canceled', 
@@ -18,12 +19,16 @@ stand_features = ['lead_time',
                   'previous_bookings_not_canceled', 'booking_changes', 
                   'days_in_waiting_list', 
                   'required_car_parking_spaces', 
-                  'total_of_special_requests'
+                  'total_of_special_requests',
+                  #'arrival_date_year',
+                  #'arrival_date_month',
+                  #'arrival_date_day_of_month',
                   ]
 
-makeNaN_features = ['company',
-                    'agent'
-                    ]
+makeNaN_list = [['company', -1],      # 0: feature, 1: label
+                ['agent', -1],
+                ['children', 0],   # 'children' in train.csv has missing entries
+                ]
 
 makeOthers_list = [['country', 'PRT', 'GBR', 'other'],        # 0: feature, 1~len-2: types, len-1: label
                    #['market_segment', 'Online TA', 'Offline TA/TO', 'other'],
@@ -32,7 +37,7 @@ makeOthers_list = [['country', 'PRT', 'GBR', 'other'],        # 0: feature, 1~le
                    ['company', -1, 40, -2],
                    ]
 
-one_hot_list = [['hotel', 'Resort Hotel', 'City Hotel'],        # 0: feature, 1~len-2: types, len-1: label
+one_hot_list = [['hotel', 'Resort Hotel', 'City Hotel'],        # 0: feature, 1~len-1: types
                 ['arrival_date_year', 2015, 2016, 2017],
                 ['arrival_date_month', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
                 ['arrival_date_day_of_month', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
@@ -49,6 +54,9 @@ one_hot_list = [['hotel', 'Resort Hotel', 'City Hotel'],        # 0: feature, 1~
                 ['company', -1, 40, -2],
                 ['customer_type', 'Contract', 'Group', 'Transient', 'Transient-party'],
                 ]
+
+encode_list = [['arrival_date_month', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],        # 0: feature, 1~len-1: types
+               ]
 
 def feature_delete_one(df, feature):
     del df[feature]
@@ -71,9 +79,9 @@ def feature_makeNaN_one(df, feature, label):
         if (pd.isnull(df.iloc[i][feature])):
             df.iat[i, df.columns.get_loc(feature)] = label
 
-def feature_makeNaN(df, features):
-    for i in range(len(features)):
-        feature_makeNaN_one(df, features[i], -1)
+def feature_makeNaN(df, list):
+    for i in range(len(list)):
+        feature_makeNaN_one(df, list[i][0], list[i][1])
 
 def feature_makeOthers_one(df, feature, types, label):
     for i in range(len(df)):
@@ -106,11 +114,51 @@ def feature_one_hot(df, list):
         one_list = list[i]
         feature_one_hot_one(df, one_list[0], one_list[1:len(one_list)])
 
+def feature_encode_one(df, feature, types):
+    column = []
+    for i in range(len(df)):
+        for j in range(len(types)):
+            if (df.iloc[i][feature] == types[j]):
+                column.append(j)
+                continue
+    
+    df.insert(len(df.columns), feature, column, True) 
+    feature_delete_one(df, feature)
+
+def feature_encode(df, list):
+    for i in range(len(list)):
+        one_list = list[i]
+        feature_encode_one(df, one_list[0], one_list[1:len(one_list)])
+
 def feature_merge_two_multi(df, feature1, feature2, name): # feature = feature1 * feature2
     column = []
     for i in range(len(df)):
         column.append(df.iloc[i][feature1] * df.iloc[i][feature2])
     
-    df.insert(len(df.columns), name, column, True) 
+    df.insert(0, name, column, True) 
     feature_delete_one(df, feature1)
     feature_delete_one(df, feature2)
+
+def preprocessing_adr():
+    data_train = pd.read_csv('../data/train.csv')
+
+    feature_delete(data_train, del_features)
+    feature_makeNaN(data_train, makeNaN_list)
+    feature_makeOthers(data_train, makeOthers_list)
+    #feature_encode(data_train, encode_list)
+    feature_one_hot(data_train, one_hot_list)
+    feature_merge_two_multi(data_train, 'is_canceled', 'adr', 'adr_real')
+    #data_train = data_train.dropna()
+    feature_standardize(data_train, stand_features)
+    data_train.to_csv('../data/train_pre_noshuffle.csv', index=False, header=False)
+    data_train = data_train.sample(frac=1) # shuffle
+
+    data_train.to_csv('../data/train_pre.csv', index=False, header=False)
+
+def preprocessing_revenue():
+    data_train = pd.read_csv('../data/train.csv')
+    data_train_label = pd.read_csv('../data/train_label.csv')
+    feature_delete_one(data_train_label, 'arrival_date')
+    revenue_no_quan = util.get_revenue_no_quan(data_train)
+    data_train_label.insert(len(data_train_label.columns), 'label_no_quan', revenue_no_quan, True) 
+    data_train_label.to_csv('../data/train_label_label_no_quan.csv', index=False, header=False)
